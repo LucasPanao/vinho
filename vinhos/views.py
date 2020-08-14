146923e5e67852e5a9d197cc74b3bb210fef29fa
LucasPanao <panaotrabalho@gmail.com>
2020-08-13 22:27:27 -0300

    Vinhos gerados dinamicamente
    Foi passado um dicionário com os nomes dos vinhos.

--- vinhos/views.py
+++ vinhos/views.py
@@ -4,4 +4,13 @@ def index(request):
     return render(request,'index.html')
 
 def listavinhos(request):
-    return render(request,'listavinhos.html')
+
+    vinhos = {
+        1:'Vinho Rapariga',
+        2:'Vinho Rapariga 2',
+        3:'Vinho Rapariga 3'
+    }
+    dados = {
+        'nomeVinho' : vinhos
+    }
+    return render(request,'listavinhos.html',dados)
