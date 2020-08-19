df612e90f2ee6f65b0422c68a204987e24080c29
LucasPanao <panaotrabalho@gmail.com>
2020-08-19 19:39:56 -0300

    Configurando banco de dados #2
    Banco Postgresql configurado, models configurado.
    Django admin configurado.
    Passando objetos do banco de dados no HTML.
    Obs: O primeiro commit não foi todos os arquivos.

--- vinhos/models.py
+++ vinhos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Vinhos(models.Model):
+    nomeVinho = models.CharField(max_length=200)
+    imagemVinho = models.ImageField(upload_to='.')
+    descVinho = models.TextField()
+    dataCadastro = models.DateTimeField(default=datetime.now,blank=True) 
--- vinhos/views.py
+++ vinhos/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
+from .models import Vinhos
 
 def index(request):
     return render(request,'index.html')
 
 def listavinhos(request):
 
-    vinhos = {
-        1:'Vinho Rapariga',
-        2:'Vinho Rapariga 2',
-        3:'Vinho Rapariga 3'
-    }
+    vinhos = Vinhos.objects.all()
+
     dados = {
-        'nomeVinho' : vinhos
+        'vinhos' : vinhos
     }
     return render(request,'listavinhos.html',dados)
